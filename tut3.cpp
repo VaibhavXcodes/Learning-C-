bf0a5b8134b133f1a9c6a80ff7b8dd2d9b1f020b
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-02-04 20:56:37 +0530

    presnent tense

---
+++ tut3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum = 6;
+    cout<<"Hello World \n"<<sum;
+    return 0;
+}
+// Single line comment 
+/* Multiline 
+comment in 
+C plus plus 
+this is tutorial 3 in cpp
+*/
---
+++ tut4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    int b = 5;
+    float pi = 3.14;
+    cout<<"This is tutorial 4.\nHere the value of a is"<<a<<".The value of b is\n"<<b<<endl;
+    cout<<"The value of pi is: "<<pi<<endl;   
+    cout<<"This is a new file for me readme.md file name"<<endl;
+    return 0;
+}
