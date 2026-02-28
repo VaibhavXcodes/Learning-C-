ae732ab1aded668bdee5957aa9b3ec8e94a0377a
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-02-28 13:35:55 +0000

    Feb 28, 2026, 7:05 PM

--- sidesoftriangle.cpp
+++ sidesoftriangle.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
+    int a,b,c;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"Enter the value of c: ";
+    cin>>c;
     
 
     
