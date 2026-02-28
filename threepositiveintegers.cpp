baa1b1ef05631ce839359a47588e5ede69f3cb67
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-02-28 14:30:26 +0000

    Feb 28, 2026, 8:00 PM

--- threepositiveintegers.cpp
+++ threepositiveintegers.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 int main()
 {
-    
-    
+    int a,b,c;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"Enter the value of c: ";
+    cin>>c;
+    if(a>b && a>c){
+        cout<<a<<endl;
+        cout<<"A is greater ";
+    }
+    else if(b>a && b>c){
+        cout<<b<<endl;
+        cout<<"B is greater ";
+    }
+    else
+        cout<<"C is greater \n"<<c;
+        
     return 0;
 }
