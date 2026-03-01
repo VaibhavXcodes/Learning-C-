73d607f6786392a91367dd119b3603b68da5d4d6
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-03-01 07:44:05 +0000

    Mar 1, 2026, 1:14 PM

--- nestedifelse.cpp
+++ nestedifelse.cpp
@@ -11,9 +11,23 @@ int main()
 
     cout<<"Enter the value of c : ";
     cin>>c;
-
-    
-
+    //! a>b and a>c --> a>c --> a greatest.
+    if(a>b){
+        if(a>c){
+            cout<<a<<" is greatest";
+        }
+        else{ // c>a , a>b --> c>a>b
+            cout<<c<<" is greates";
+        }
+    }
+    else{ //b>a
+        if(b>c){
+            cout<<b<<" is greatest";
+        }
+        else{ //c >b , c > b > a
+            cout<<c<<" is greatest";
+        }
+    }
     
     return 0;
 }
