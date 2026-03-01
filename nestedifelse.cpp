dc3f402c0b421a97b54a0d160b393180842a0e19
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-03-01 07:31:12 +0000

    Mar 1, 2026, 1:01 PM

---
+++ nestedifelse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    
+    cout<<"Enter the value of b : ";
+    cin>>b;
+
+    cout<<"Enter the value of c : ";
+    cin>>c;
+
+    
+
+    
+    return 0;
+}
