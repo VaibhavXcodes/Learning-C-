6763cbae2c260a95106080b9b3c513d0b1e5cb80
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-03-05 04:30:40 +0000

    Mar 5, 2026, 10:00 AM

--- gradeandmarks.cpp
+++ gradeandmarks.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 int main()
 {
-    
-
+    int n;
+    cout<<"Enter marks : ";
+    cin>>n;
+    // if(n>=81 && n<=100){
+    //     cout<<"Very Good";
+    // }
+    // if(n>=61 && n<=80){
+    //     cout<<"Good";
+    // }
+    // if(n>=41 && n<=60){
+    //     cout<<"Average";
+    // }
+    // if(n<=40){
+    //     cout<<"Fail";
+    // }
+    if(n>=81 && n<=100){
+        cout<<"Very Good";
+    }
+    else if(n>=61 && n<=80){
+        cout<<"Good";
+    }
+    else if(n>=41 && n<=60){
+        cout<<"Average";
+    }
+    else{
+        cout<<"Fail";
+    }
     return 0;
 }
