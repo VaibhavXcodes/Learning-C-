66111bb95b96b9ec8da24d3b0d5524f6eb0fa18d
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-03-05 05:09:58 +0000

    Mar 5, 2026, 10:39 AM

--- calculator.cpp
+++ calculator.cpp
@@ -4,5 +4,11 @@ int main(){
     int n1;
     cin>>n1;
     char op;
-    
+    cin>>op;
+    int n2;
+    cin>>n2;
+    if(op=='+') cout<<n1+n2;
+    if(op == '-') cout<<n1 - n2;
+    if(op=='*') cout<<n1*n2;
+    if(op == '/') cout<<n1 / n2;
 }
