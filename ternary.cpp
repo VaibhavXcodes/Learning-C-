24235f2dd92fef36c6f960543e5091ac1dfae829
Vaibhav Sharma <vaibhav0773@gmail.com>
2026-03-05 04:52:33 +0000

    Mar 5, 2026, 10:22 AM

--- ternary.cpp
+++ ternary.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    // (condition) ? if true : if false ;
+    (n%2==0) ? cout<<"even number" : cout<<"odd number";
+    
     return 0;
 }
